2885037de18bdabeb2b2a4449b1aeea1605b7ef6
Дмитрий <dmitrij@MacBook-Air.local>
2026-09-23 16:43:44 +0700

    fix: clone exact release tags for all core dependencies

--- buildScript/lib/core/get_source.sh
+++ buildScript/lib/core/get_source.sh
@@ -11,33 +11,29 @@ MASTER_DNS_PATCH=$(realpath "patches/core/MasterDnsVPN.patch")
 pushd ..
 #### sing-box
 if [ ! -d "sing-box" ]; then
-  git clone --depth 50 https://github.com/SagerNet/sing-box.git sing-box
+  git clone --depth 1 --branch v1.14.0 https://github.com/SagerNet/sing-box.git sing-box
   pushd sing-box
-  git checkout 0b8995879f29a9b98ee027bc17b75e101445b238
   git apply "$SING_BOX_PATCH"
   popd
 fi
 #### sing-vmess
 if [ ! -d "sing-vmess" ]; then
-  git clone --depth 50 https://github.com/starifly/sing-vmess.git sing-vmess
+  git clone --depth 1 --branch dev https://github.com/starifly/sing-vmess.git sing-vmess
   pushd sing-vmess
-  git checkout 887f058c04c5b86d88188acc32093d0d62f53b6c
   git apply "$SING_VMESS_PATCH"
   popd
 fi
 #### libneko
 if [ ! -d "libneko" ]; then
-  git clone --depth 50 https://github.com/starifly/libneko.git libneko
+  git clone --depth 1 --branch main https://github.com/starifly/libneko.git libneko
   pushd libneko
-  git checkout 6a85c185d62435a5293ef70ac3b638ae3ee1efa7
   git apply "$LIBNEKO_PATCH"
   popd
 fi
 #### MasterDnsVPN-plus
 if [ ! -d "MasterDnsVPN-plus" ]; then
-  git clone https://github.com/masterking32/MasterDnsVPN.git MasterDnsVPN-plus
+  git clone --depth 1 --branch v2026.06.13.234407-7de2476 https://github.com/masterking32/MasterDnsVPN.git MasterDnsVPN-plus
   pushd MasterDnsVPN-plus
-  git checkout 7de2476f1c33e69eec35360c52810dab43c5c986
   git apply "$MASTER_DNS_PATCH"
   popd
 fi
